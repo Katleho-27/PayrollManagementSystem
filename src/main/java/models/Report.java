a3d1de5da39d072d6d39ba0da2ddf54bc7025823
Katleho-27 <kmphatsoane47@gmail.com>
2025-05-08 15:47:06 +0200

    Delete src/main directory

--- src/main/java/models/Report.java
+++
@@ -1,9 +0,0 @@
-package models;
-
-public class Report {
-    private double totalPayrollExpense;
-    private double totalDeductions;
-    private double totalNetSalary;
-
-    // Constructor, Getters, and Setters
-}
